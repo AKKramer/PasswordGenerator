06f85ce61c422e61bda3bd2dd5d2f4b021ae2664
AKKramer <austinkkramer@gmail.com>
2020-06-10 20:14:54 -0500

    Initial upload. Coding plan and random generators complete.

---
+++ Develop/script.js
@@ -0,0 +1,98 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//User clicks the button asking to generate a password
+
+// Button is listening for the user's click. When clicked it calls the next appropriate function.
+
+//PROMPT asks the user how long they would like their password to be. Between 8-128 characters. 
+
+// Validation checks to see if the number inputted is less than 8 or greater than 128. If so, loop and keep asking user the same question. 
+
+// -------------------------------------
+
+// CONFIRM asks the user if they would like to include LOWER CASE letters in their password. 
+
+// Lower case confirmation variable is changed to TRUE
+
+
+// CONFIRM asks the user if they would like to include UPPER CASE letters in their password. 
+
+// Upper case confirmation variable is changed to TRUE
+
+
+// CONFIRM asks the user if they would like to include NUMBERS in their password. 
+
+// Numbers confirmation variable is changed to TRUE
+
+
+// CONFIRM asks the user if they would like to include SYMBOLS in their password. 
+
+// Symbols confirmation variable is changed to TRUE
+
+
+
+// Validation checks to verify that at least one of the prompts above was answered TRUE.
+
+// ---------------------------------------
+
+
+// The four password generator functions are put into an array.
+
+// A for-loop is started, looping equal to the length of the password the user entered. 
+
+// Using Math.random, the four functions are randomly called inside the for-loop to input a value into a separate password array.
+
+// The password array is printed to the webpage. 
+
+
+
+
+
+
+//Generate random letter functions
+function getRandomLower(){
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  return lowerCase [Math.floor(Math.random() * lowerCase.length)]
+
+}
+
+function getRandomUpper(){
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  return upperCase [Math.floor(Math.random() * upperCase.length)]
+
+}
+
+function getRandomNumber(){
+  var numbers = '1234567890';
+  return numbers [Math.floor(Math.random() * numbers.length)];
+
+}
+
+function getRandomSymbol() {
+  var symbols = '!@#$%^&*()-+';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+  console.log(symbols.length)
+
+}
+
+
+console.log(getRandomLower())
+console.log(getRandomUpper())
+console.log(getRandomNumber())
+console.log(getRandomSymbol())
+
+
