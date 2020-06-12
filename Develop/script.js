cf332b6ea55c7cc4c1f08310006729d6c5a25cbe
AKKramer <austinkkramer@gmail.com>
2020-06-12 08:51:05 -0500

    As the user selects the password preferences, each password generator function is added to array passwordCreaterArr

--- Develop/script.js
+++ Develop/script.js
@@ -35,55 +35,74 @@ while (passwordLength < 8 || passwordLength > 128) {
 // Create and array to store user selector password preferences
 
 var passwordSettingsArr = []
+var passwordCreaterArr = []
 
 allPasswordSettings()
 
 // Create a function to store the password preferenaces below
 function allPasswordSettings (){
 
-// CONFIRM asks the user if they would like to include LOWER CASE letters in their password. 
-var lowerCase = confirm("Would you like your password to include lower case letters?")
-  console.log(lowerCase)
-  
+  // CONFIRM asks the user if they would like to include LOWER CASE letters in their password. 
+  var lowerCase = confirm("Would you like your password to include lower case letters?")
+    console.log(lowerCase)
+    
 
-// Lower case confirmation variable is changed to TRUE
-passwordSettingsArr.push(lowerCase)
+  // Lower case confirmation variable is changed to TRUE or FALSE and then pushed into an array.
+  passwordSettingsArr.push(lowerCase)
+  // If the prefrence is selected, the random generater for that preference is added to an array. 
+    if (lowerCase === true) {
+      passwordCreaterArr.push(getRandomLower)
+    }
   console.log(passwordSettingsArr)
-
-  
-// CONFIRM asks the user if they would like to include UPPER CASE letters in their password. 
-var upperCase = confirm("Would you like your password to include upper case letters")
-  console.log(upperCase)
-
-
-// Upper case confirmation variable is changed to TRUE
-passwordSettingsArr.push(upperCase)
-  console.log(passwordSettingsArr)
+  console.log(passwordCreaterArr)
+    
+  // CONFIRM asks the user if they would like to include UPPER CASE letters in their password. 
+  var upperCase = confirm("Would you like your password to include upper case letters")
+    console.log(upperCase)
 
 
-// CONFIRM asks the user if they would like to include NUMBERS in their password. 
-var numbers = confirm("Would you like your password to include numbers?")
-  console.log(numbers)
+  // Upper case confirmation variable is changed to TRUE or FALSE and then pushed into an array.
+  passwordSettingsArr.push(upperCase)
+  // If the prefrence is selected, the random generater for that preference is added to an array.
+    if (upperCase === true) {
+      passwordCreaterArr.push(getRandomUpper)
+    }
+    console.log(passwordSettingsArr)
+    console.log(passwordCreaterArr)
 
 
-// Numbers confirmation variable is changed to TRUE
-passwordSettingsArr.push(numbers)
-  console.log(passwordSettingsArr)
+  // CONFIRM asks the user if they would like to include NUMBERS in their password. 
+  var numbers = confirm("Would you like your password to include numbers?")
+    console.log(numbers)
 
 
-// CONFIRM asks the user if they would like to include SYMBOLS in their password. 
-var symbols = confirm("Would you like your password to include symbols?")
-  console.log(symbols)
+  // Numbers confirmation variable is changed to TRUE or FALSE and then pushed into an array.
+  passwordSettingsArr.push(numbers)
+  // If the prefrence is selected, the random generater for that preference is added to an array.
+    if (numbers === true) {
+      passwordCreaterArr.push(getRandomNumber)
+    }
+    console.log(passwordSettingsArr)
+    console.log(passwordCreaterArr)
 
 
-// Symbols confirmation variable is changed to TRUE
-passwordSettingsArr.push(symbols)
-  console.log(passwordSettingsArr)
+  // CONFIRM asks the user if they would like to include SYMBOLS in their password. 
+  var symbols = confirm("Would you like your password to include symbols?")
+    console.log(symbols)
 
-// Calls the validation function to make sure at least one of the answers above was true. 
-passwordValidation()  
+
+  // Symbols confirmation variable is changed to TRUE or FALSE and then pushed into an array.
+  passwordSettingsArr.push(symbols)
+    console.log(passwordSettingsArr)
+      if (symbols === true) {
+        passwordCreaterArr.push(getRandomSymbol)
+      }
+
+  // Calls the validation function to make sure at least one of the answers above was true. 
+  passwordValidation()  
 }
 
+console.log(passwordCreaterArr)
 // ---------------------------------------------------------------------------
 
 // Validation checks to verify that at least one of the prompts above was answered TRUE.
@@ -107,8 +126,18 @@ function passwordValidation () {
 
 console.log(passwordSettingsArr)
 
+// The selected password generator functions are now in array "passwordCreaterArr".
 
+// A for-loop is started, looping equal to the length of the password the user entered. 
+// Using Math.random, the four functions are randomly called inside the for-loop to input a value into a separate password array.
 
+iconcycle[Math.floor(Math.random() * iconcycle.length)]();
+
+// The password array is printed to the webpage
+
+// TODO: function generatePassword () {
+//   for 
+// }
 
 
 
